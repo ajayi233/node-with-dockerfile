c4c463330ce0a4bcdc259fd0243509c28c6baed1
Lotsu Daniel Ajayi <daniel.lotsu@amalitech.com>
2025-03-18 08:08:56 +0000

    testing CI/CD

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -11,5 +11,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
